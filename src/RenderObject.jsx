7a0f91fa474c90e125f53ede060fd7e4b10d6e83
paavantech <contact@paavan.app>
2023-08-24 22:02:04 +0530

    Minor Fixes

--- src/RenderObject.jsx
+++ src/RenderObject.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 import data from "./data.json";
 import {Graphics, Sprite, Stage, Text} from "@inlet/react-pixi";
 import * as PIXI from "pixi.js";
@@ -7,52 +7,82 @@ import "./global.css";
 const RenderObject = () => {
     const {data: data_} = data;
     const [objects, setObjects] = React.useState(data_);
+    const [showData, setShowData] = React.useState(false)
+    const [isMobile, setIsMobile] = React.useState(false)
+
+    React.useEffect(() => {
+        const handleResize = () => {
+            if (window.screen.width < 400) {
+                setIsMobile(true)
+            } else {
+                setIsMobile(false)
+
+            }
+        }
+
+        window.addEventListener('resize', handleResize)
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
     return (
         <div
         >
-            <Stage width={400} height={300} options={{backgroundColor: 0x1099bb}}>
+            {isMobile ? "Mobile" : "PC"}
+            <Stage
+                // width={400}
+                height={300}
+                width={window.innerWidth}
+                // height={window.innerHeight}
+                options={{backgroundColor: 0x1099bb}}>
                 {objects.map((object, index) => {
                     if (object.type == 'rectangle') {
-                        return <RenderRectangle data={object} index={index}/>
+                        return <RenderRectangle data={object} isMobile={isMobile} index={index}/>
                     }
                 })}
             </Stage>
 
-            <div
+            <button
+                onClick={() => {
+                    setShowData(!showData)
+                }}
                 style={{
                     display: "flex",
                     flexDirection: "row",
                     flexWrap: "wrap",
                 }}
             >
-                {objects.map((object, index) => {
-                    return (
-                        <div
-                            key={index}
+                {showData ? "Hide" : "Show"} Data
+            </button>
+
+            {showData && objects.map((object, index) => {
+                return (
+                    <div
+                        key={index}
+                        style={{
+                            border: "1px solid black",
+                            padding: "10px",
+                        }}
+                    >
+                        <h3>
+                            {object.type}-{object.id}
+                        </h3>
+                        <pre
                             style={{
-                                border: "1px solid black",
-                                padding: "10px",
+                                fontSize: "20px",
                             }}
                         >
-                            <h3>
-                                {object.type}-{object.id}
-                            </h3>
-                            <pre
-                                style={{
-                                    fontSize: "20px",
-                                }}
-                            >
-                                {JSON.stringify(object, null, 2)}
-                            </pre>
-                        </div>
-                    )
-                })}
-            </div>
+                            {JSON.stringify(object, null, 2)}
+                        </pre>
+                    </div>
+                )
+            })}
         </div>
     )
 };
 
-const RenderRectangle = ({data, index}) => {
+const RenderRectangle = ({data, index, isMobile}) => {
     const {
         id,
         type,
@@ -73,6 +103,8 @@ const RenderRectangle = ({data, index}) => {
         y: object.y
     });
 
+    const scaleFactor = isMobile ? 1 / 3 : 1;
+
     if (!is_visible) {
         return null;
     }
@@ -83,6 +115,9 @@ const RenderRectangle = ({data, index}) => {
         let isHeightFixed = false;
         let isWidthFixed = false;
 
+        deltaX *= scaleFactor;
+        deltaY *= scaleFactor;
+
         if (fixed_sides) {
             if (fixed_sides.includes('l')) {
                 isHeightFixed = true;
@@ -91,25 +126,18 @@ const RenderRectangle = ({data, index}) => {
             }
         }
 
+        // //Can't drag further than where it was
+        // const currentHeight = newSize.height;
+        // const currentWidth = newSize.width;
+        //
+        // if (currentWidth < 10) {
+        //     return;
+        // }
+        //
+        // if (currentHeight < 10) {
+        //     return;
+        // }
 
-        if (limit) {
-            const currentHeight = newSize.height;
-            const currentWidth = newSize.width;
-            if (limit['l']) {
-                if (currentHeight + deltaY < limit['l'][0]) {
-                    return;
-                } else if (currentHeight + deltaY > limit['l'][1]) {
-                    return;
-                }
-            }
-            if (limit['b']) {
-                if (currentWidth + deltaX < limit['b'][0]) {
-                    return;
-                } else if (currentWidth + deltaX > limit['b'][1]) {
-                    return;
-                }
-            }
-        }
 
         if (resizeable_options) {
             const {type} = resizeable_options;
@@ -125,12 +153,16 @@ const RenderRectangle = ({data, index}) => {
                         case 0: // Top-left corner
                             newSize.width -= stepAlignedDeltaX;
                             newSize.height -= stepAlignedDeltaY;
+
                             // newPosition.x += stepAlignedDeltaX;
                             // newPosition.y += stepAlignedDeltaY;
                             break;
                         case 1: // Top-right corner
                             newSize.width += stepAlignedDeltaX;
                             newSize.height -= stepAlignedDeltaY;
+                            if (newSize.width < 10) {
+                                newSize.width = 10;
+                            }
                             // newPosition.y += stepAlignedDeltaY;
                             break;
                         case 2: // Bottom-right corner
@@ -145,6 +177,25 @@ const RenderRectangle = ({data, index}) => {
                         default:
                             break;
                     }
+
+                    if (limit) {
+                        if (newSize.height < limit['l'][0]) {
+                            newSize.height = limit['l'][0]
+                        } else if (newSize.height > limit['l'][1]) {
+                            newSize.height = limit['l'][1]
+                        } else if (newSize.height < 5) {
+                            newSize.height = 5;
+                        }
+
+                        if (newSize.width < limit['b'][0]) {
+                            newSize.width = limit['b'][0]
+                        } else if (newSize.width > limit['b'][1]) {
+                            newSize.width = limit['b'][1]
+                        } else if (newSize.width < 5) {
+                            newSize.width = 5;
+                        }
+                    }
+
                     setRectSize(newSize);
                     return;
                 }
@@ -182,42 +233,49 @@ const RenderRectangle = ({data, index}) => {
     };
 
     const getResizeablePointX = (cornerIndex) => {
-        if (fixed_corner_index != null) {
-            switch (fixed_corner_index) {
-                case 0:
-                    return rectSize.x + (cornerIndex === 0 || cornerIndex === 3 ? 0 : rectSize.width);
-                case 1:
-                    return rectSize.x + (cornerIndex === 0 || cornerIndex === 3 ? -1 * rectSize.width : 0);
-                case 2:
-                    return rectSize.x + (cornerIndex === 0 || cornerIndex === 3 ? -1 * rectSize.width : 0);
-                case 3:
-                    return rectSize.x + (cornerIndex === 0 || cornerIndex === 3 ? 0 : rectSize.width);
-                default:
-                    break;
+        const getResizeablePointXValue = () => {
+            if (fixed_corner_index != null) {
+                switch (fixed_corner_index) {
+                    case 0:
+                        return rectSize.x + (cornerIndex === 0 || cornerIndex === 3 ? 0 : rectSize.width);
+                    case 1:
+                        return rectSize.x + (cornerIndex === 0 || cornerIndex === 3 ? -1 * rectSize.width : 0);
+                    case 2:
+                        return rectSize.x + (cornerIndex === 0 || cornerIndex === 3 ? -1 * rectSize.width : 0);
+                    case 3:
+                        return rectSize.x + (cornerIndex === 0 || cornerIndex === 3 ? 0 : rectSize.width);
+                    default:
+                        break;
+                }
+            } else {
+                return rectSize.x + (cornerIndex === 0 || cornerIndex === 3 ? 0 : rectSize.width);
             }
-        } else {
-            return rectSize.x + (cornerIndex === 0 || cornerIndex === 3 ? 0 : rectSize.width);
-
         }
+        return getResizeablePointXValue() * scaleFactor;
+
     };
 
     const getResizeablePointY = (cornerIndex) => {
-        if (fixed_corner_index != null) {
-            switch (fixed_corner_index) {
-                case 0:
-                    return rectSize.y + (cornerIndex === 0 || cornerIndex === 1 ? 0 : rectSize.height);
-                case 1:
-                    return rectSize.y + (cornerIndex === 0 || cornerIndex === 1 ? 0 : rectSize.height);
-                case 2:
-                    return rectSize.y + (cornerIndex === 0 || cornerIndex === 1 ? -1 * rectSize.height : 0);
-                case 3:
-                    return rectSize.y + (cornerIndex === 0 || cornerIndex === 1 ? -1 * rectSize.height : 0);
-                default:
-                    break;
+        const getPoint = ()=>{
+            if (fixed_corner_index != null) {
+                switch (fixed_corner_index) {
+                    case 0:
+                        return rectSize.y + (cornerIndex === 0 || cornerIndex === 1 ? 0 : rectSize.height);
+                    case 1:
+                        return rectSize.y + (cornerIndex === 0 || cornerIndex === 1 ? 0 : rectSize.height);
+                    case 2:
+                        return rectSize.y + (cornerIndex === 0 || cornerIndex === 1 ? -1 * rectSize.height : 0);
+                    case 3:
+                        return rectSize.y + (cornerIndex === 0 || cornerIndex === 1 ? -1 * rectSize.height : 0);
+                    default:
+                        break;
+                }
+            } else {
+                return rectSize.y + (cornerIndex === 0 || cornerIndex === 1 ? 0 : rectSize.height);
             }
-        } else {
-            return rectSize.y + (cornerIndex === 0 || cornerIndex === 1 ? 0 : rectSize.height);
         }
+
+        return getPoint() * scaleFactor;
     }
 
     return (
@@ -229,19 +287,31 @@ const RenderRectangle = ({data, index}) => {
                     g.clear();
                     g.lineStyle(2, 0x000000, 1);
                     g.beginFill(object.fill, 0.5);
+
+                    // Adjust dimensions by the scale factor
+                    const adjustedX = rectSize.x * scaleFactor;
+                    const adjustedY = rectSize.y * scaleFactor;
+                    const adjustedWidth = rectSize.width * scaleFactor;
+                    const adjustedHeight = rectSize.height * scaleFactor;
+
                     if (fixed_corner_index !== null) {
                         switch (fixed_corner_index) {
                             case 0:
-                                g.drawRect(rectSize.x, rectSize.y, rectSize.width, rectSize.height);
+                                // g.drawRect(rectSize.x, rectSize.y, rectSize.width, rectSize.height);
+                                g.drawRect(adjustedX, adjustedY, adjustedWidth, adjustedHeight);
                                 break;
                             case 1:
-                                g.drawRect(rectSize.x - rectSize.width, rectSize.y, rectSize.width, rectSize.height);
+                                // g.drawRect(rectSize.x - rectSize.width, rectSize.y, rectSize.width, rectSize.height);
+                                g.drawRect(adjustedX - adjustedWidth,adjustedY, adjustedWidth, adjustedHeight)
                                 break;
                             case 2:
-                                g.drawRect(rectSize.x - rectSize.width, rectSize.y - rectSize.height, rectSize.width, rectSize.height);
+                                // g.drawRect(rectSize.x - rectSize.width, rectSize.y - rectSize.height, rectSize.width, rectSize.height);
+                                g.drawRect(adjustedX - adjustedWidth, adjustedY - adjustedHeight, adjustedWidth, adjustedHeight);
+
                                 break;
                             case 3:
-                                g.drawRect(rectSize.x, rectSize.y - rectSize.height, rectSize.width, rectSize.height);
+                                // g.drawRect(rectSize.x, rectSize.y - rectSize.height, rectSize.width, rectSize.height);
+                                g.drawRect(adjustedX, adjustedY - adjustedHeight, adjustedWidth, adjustedHeight);
                                 break;
                             default:
                                 break;
@@ -322,7 +392,7 @@ const RenderRectangle = ({data, index}) => {
                                 // y={rectSize.y + (cornerIndex === 0 || cornerIndex === 1 ? -1 * rectSize.height / 2 : rectSize.height / 2)}
                                 // Font size
                                 style={{
-                                    fontSize: 10,
+                                    fontSize: isMobile ? 5 :10
                                 }}
 
                             />
